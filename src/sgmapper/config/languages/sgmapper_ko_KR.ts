2ad06a22510ed060ec79a8fdf68c71b239977070
dounggiduk <dounggiduk@gmail.com>
2018-08-03 17:49:11 +0900

    add ts files

---
+++ src/sgmapper/config/languages/sgmapper_ko_KR.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ko_KR">
+<context>
+    <name>GQManagerWidget</name>
+    <message>
+        <source>sgMapper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Print...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Upload...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Setting...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tiling map...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tiling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Build pyramids overviews...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Overviews</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About AAIIS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>pan...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pan...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Distance...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Area...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
