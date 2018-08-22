d726ee6b9f91213e3922638b78c46f057692e82c
dounggiduk <dounggiduk@gmail.com>
2018-08-22 16:05:51 +0900

    add languages/sgi-qt_ko_KR.ts

---
+++ src/sg-plugin/qt/config/languages/sgi-qt_ko_KR.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ko_KR">
+<context>
+    <name>QApplication</name>
+    <message>
+        <source>&amp;PlugIn</source>
+        <translation>&amp;&amp;플러그인</translation>
+    </message>
+    <message>
+        <source>demo</source>
+        <translation>데모</translation>
+    </message>
+</context>
+<context>
+    <name>mDlgMenubase</name>
+    <message>
+        <source>Menubase dialog</source>
+        <translation>메뉴다이알로그</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>확인</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>닫기</translation>
+    </message>
+</context>
+</TS>
